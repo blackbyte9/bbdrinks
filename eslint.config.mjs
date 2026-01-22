00aecd97b413065ded8707badfcd1a7832ae5279
Black Byte <blackbyte9@gmx.net>
2026-01-22 19:27:28 +0100

    Create some preliminary display elements

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,7 +12,21 @@ const eslintConfig = defineConfig([
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "src/components/ui/**",
+    "src/lib/utils.ts",
   ]),
+  {
+    rules: {
+      semi: "error",
+      "no-trailing-spaces": "error",
+      "no-duplicate-imports": "error",
+      "eol-last": ["error", "always"],
+      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }],
+      "no-trailing-spaces": "error",
+      "no-console": "warn", // Allow console statements, but warn about them
+      "no-debugger": "warn", // Allow debugger statements, but warn about them
+    },
+  },
 ]);
 
 export default eslintConfig;
